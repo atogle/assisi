7450198712e8a048afd0d729e06d6d809d89d482
Aaron Ogle <atogle@gmail.com>
2015-01-24 23:18:24 -0500

    Redirect root to admin (for now).

--- src/web/urls.py
+++ src/web/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index_view),
-    url(r'^admin/$', views.admin_view),
+    url(r'^admin/$', views.admin_view, name='app-admin'),
     url(r'^signin/$', 'django.contrib.auth.views.login', name='app-signin', kwargs={'template_name': 'signin.html'}),
     url(r'^signout/$', 'django.contrib.auth.views.logout', name='app-signout', kwargs={'next_page': '/'}),
 )
--- src/web/views.py
+++ src/web/views.py
@@ -1,9 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 
 
 def index_view(request):
-    return render_to_response('index.html')
+    return redirect('app-admin')
 
 
 @login_required
