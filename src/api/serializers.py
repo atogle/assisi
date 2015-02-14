08b963f464c94107e181abde40babf693ce155cf
Aaron Ogle <atogle@gmail.com>
2015-02-14 15:38:20 -0500

    Do duplicate checking on the server side.

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,5 +1,5 @@
 from .models import Request
-from rest_framework import serializers
+from rest_framework import serializers, validators
 
 
 class RequestSerializer(serializers.HyperlinkedModelSerializer):
@@ -7,3 +7,10 @@ class RequestSerializer(serializers.HyperlinkedModelSerializer):
         model = Request
         fields = ('id', 'name', 'address', 'apt', 'city', 'state', 'zip',
                   'distribution_site', 'email', 'phone', 'phone_type')
+        validators = [
+            validators.UniqueTogetherValidator(
+                queryset=Request.objects.all(),
+                fields=('address', 'apt', 'zip'),
+                message='Someone has already made a request for this address (address, apt, and zip).'
+            )
+        ]
