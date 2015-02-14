8a6288951450a8853d71132fe07d9492ae94b9a3
Aaron Ogle <atogle@gmail.com>
2015-02-14 12:01:46 -0500

    Boostrap partner data into the admin template. - Add config.yml file for partner data. - Parse and add data to the admin template context. - Render partner data to a JS variable in the template. - Add filter for rendering json in a template. - Add yaml dependency.

--- src/web/views.py
+++ src/web/views.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import render, redirect
+import json
+import os
+import yaml
+
+
+def get_config(filename):
+    config_file = open(filename)
+    try:
+        config_yml = yaml.load(config_file)
+    finally:
+        config_file.close()
+
+    return config_yml
 
 
 def index_view(request):
@@ -8,4 +21,11 @@ def index_view(request):
 
 @login_required
 def admin_view(request):
-    return render_to_response('admin.html')
+    path = os.path.abspath(os.path.dirname(__file__))
+    fileepath = os.path.join(path, 'config.yml')
+    config_yml = get_config(fileepath)
+    context = {
+        'partners': config_yml['partners']
+    }
+
+    return render(request, 'admin.html', context)
