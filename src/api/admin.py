ce266b879573c3e57314bd0a41b7fefc432b5db7
Aaron Ogle <atogle@gmail.com>
2016-02-11 13:42:29 -0500

    Add initial admin customizations for the API app.

---
+++ src/api/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import DistributionSite, Event, EventDistributionSiteDetails, Request
+
+
+@admin.register(Request)
+class RequestAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'apt', 'zip', 'event_distribution_site_details')
+
+
+@admin.register(EventDistributionSiteDetails)
+class EventDistributionSiteDetailsAdmin(admin.ModelAdmin):
+    list_display = ('event', 'distribution_site', 'max_requests')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active')
+
+
+@admin.register(DistributionSite)
+class DistributionSiteAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user_count')
